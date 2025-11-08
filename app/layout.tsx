8c97e2e1202b6e6f49991fa221ee1b887294a2f0
tvindima <tiagovindima@gmail.com>
2025-11-08 00:55:34 +0000

    Remove persistent sidebar from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import './globals.css';
-import Sidebar from '../components/Sidebar';
 import type { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex">
-        <Sidebar />
-        <main className="flex-1 p-4">{children}</main>
+      <body>
+        {children}
       </body>
     </html>
   );
